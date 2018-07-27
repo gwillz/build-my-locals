11f0f418d97b69e621fa8392aa1a3a2ce7d4a1ba
gwillz <gwillz@internode.on.net>
2018-07-27 22:12:58 +0930

    Added 'fail' test

--- test.js
+++ test.js
@@ -3,21 +3,39 @@ const test = require('tape')
 const fs = require('fs')
 const build = require('./index')
 
-test("Example project", assert => {
+test("Successful build", assert => {
     clean();
     
     build({
-        target: './test/package.json',
+        target: './test/package-pass.json',
     })
     .then(code => {
         assert.ok(code === 0, "exits with zero");
         
-        assert.ok(fs.existsSync('test/sub1/yes-it-worked.txt'));
-        assert.ok(fs.existsSync('test/sub2/yes-it-worked.txt'));
+        assert.ok(fs.existsSync('test/sub1/yes-it-worked.txt'), "sub1 built");
+        assert.ok(fs.existsSync('test/sub2/yes-it-worked.txt'), "sub2 built");
         
         assert.end();
     })
 })
+
+
+test("Failed build", assert => {
+    clean();
+    
+    build({
+        target: './test/package-fail.json',
+    })
+    .then(code => {
+        assert.ok(code > 0, "exits with non-zero");
+        
+        assert.ok(fs.existsSync('test/sub1/yes-it-worked.txt'), "sub1 built");
+        assert.notOk(fs.existsSync('test/sub2/yes-it-worked.txt'), "sub2 did not build");
+        
+        assert.end();
+    })
+})
+
 
 // TODO test command parameters
 
